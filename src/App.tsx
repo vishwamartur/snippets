f2a2365e35e546608c78930c4ace5eaaa9996acb
seveibar <seveibar@gmail.com>
2024-09-24 18:08:13 -0700

    tailwind working

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { useState } from "react"
 
 function App() {
-  return <div>Hello world</div>
+  return (
+    <div className="flex">
+      <div className="w-1/2"></div>
+      <div className="w-1/2"></div>
+    </div>
+  )
 }
 
 export default App
