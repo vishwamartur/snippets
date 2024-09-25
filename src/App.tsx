159b592ef227bd41d03d7dbfd8b4daeaad0830a6
seveibar <seveibar@gmail.com>
2024-09-24 20:13:12 -0700

    init 3d viewer

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,22 @@ import { CodeEditor } from "./components/CodeEditor"
 import * as Babel from "@babel/standalone"
 import * as React from "react"
 import { Circuit } from "@tscircuit/core"
+import { PCBViewer } from "@tscircuit/pcb-viewer"
+import { CadViewer } from "@tscircuit/3d-viewer"
+
+const defaultCode = `
+export default () => (
+	<board width="10mm" height="10mm">
+		<resistor resistance="1k" footprint="0402" name="R1" pcbX={3} />
+		<capacitor capacitance="1000pF" footprint="0402" name="C1" pcbX={-3} />
+		<trace from=".R1 > .pin1" to=".C1 > .pin1" />
+	</board>
+)
+`.trim()
 
 function App() {
-  const [code, setCode] = useState("")
+  const [selectedTab, setSelectedTab] = useState<"pcb" | "cad">("pcb")
+  const [code, setCode] = useState(defaultCode)
 
   const compiledCode = useMemo(() => {
     try {
@@ -21,9 +34,9 @@ function App() {
     }
   }, [code])
 
-  const { compiledModule, message } = useMemo(() => {
+  const { compiledModule, message, circuitJson } = useMemo(() => {
     try {
-      const exports = {}
+      const exports: any = {}
       const module = { exports }
       eval(compiledCode)
 
@@ -32,49 +45,58 @@ function App() {
 
         const primaryKey = Object.keys(module.exports)[0]
 
-        // circuit.add(module.exports[primaryKey])
+        circuit.add(React.createElement(module.exports[primaryKey]))
 
-        return { compiledModule: module, message: `Primary key: ${primaryKey}` }
+        circuit.render()
+
+        const circuitJson = circuit.getCircuitJson()
+
+        return {
+          compiledModule: module,
+          message: `Primary key: ${primaryKey}`,
+          circuitJson,
+        }
       } catch (error: any) {
         console.error("Evaluation error:", error)
-        return { compiledModule: module, message: `Error: ${error.message}` }
+        return {
+          compiledModule: module,
+          message: `Error: ${error.message}`,
+          circuitJson: null,
+        }
       }
 
-      return { compiledModule: module, message: "" }
+      return { compiledModule: module, message: "", circuitJson: null }
     } catch (error: any) {
       console.error("Evaluation error:", error)
-      return { compiledModule: null, message: `Error: ${error.message}` }
+      return {
+        compiledModule: null,
+        message: `Error: ${error.message}`,
+        circuitJson: null,
+      }
     }
   }, [compiledCode])
 
   return (
     <div className="flex">
       <div className="w-1/2 p-8">
-        <CodeEditor
-          defaultCode={`
-					
-export default MyCircuit = () => (
-	<board width="10mm" height="10mm">
-		<resistor resistance="1k" footprint="0402" name="R1" />
-	</board>
-)
-					
-					`.trim()}
-          onCodeChange={setCode}
-        />
+        <CodeEditor defaultCode={defaultCode} onCodeChange={setCode} />
       </div>
       <div className="w-1/2 p-8">
+        <div>
+          <button className="" onClick={() => setSelectedTab("pcb")}>
+            pcb
+          </button>
+          <button className="" onClick={() => setSelectedTab("cad")}>
+            3d
+          </button>
+        </div>
+        <div className="mt-4 h-[500px]">
+          {selectedTab === "pcb" && <PCBViewer soup={circuitJson} />}
+          {selectedTab === "cad" && <CadViewer soup={circuitJson as any} />}
+        </div>
         <textarea className="w-full h-64" value={compiledCode} />
         <hr />
         <textarea className="w-full h-8 bg-red-100" value={message} />
-        <textarea
-          className="w-full h-64 bg-gray-100"
-          value={JSON.stringify(
-            Object.keys(compiledModule?.exports ?? {}),
-            null,
-            "  ",
-          )}
-        />
       </div>
     </div>
   )
