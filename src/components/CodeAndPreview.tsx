ca1d2ad1cbb8957ed994c771d8b4cb9bf4d12c1f
seveibar <seveibar@gmail.com>
2024-09-25 14:58:24 -0700

    new nav

--- src/components/CodeAndPreview.tsx
+++ src/components/CodeAndPreview.tsx
@@ -9,7 +9,8 @@ import { defaultCodeForBlankPage } from "@/lib/defaultCodeForBlankCode"
 import { decodeUrlHashToText } from "@/lib/decodeUrlHashToText"
 import { encodeTextToUrlHash } from "@/lib/encodeTextToUrlHash"
 import { Button } from "@/components/ui/button"
-import {useRunTsx} from "@/hooks/use-run-tsx"
+import { useRunTsx } from "@/hooks/use-run-tsx"
+import EditorNav from "./EditorNav"
 
 export function CodeAndPreview() {
   const defaultCode =
@@ -19,44 +20,37 @@ export function CodeAndPreview() {
   const { message, circuitJson } = useRunTsx(code)
 
   return (
-    <div className="flex">
-      <div className="w-1/2 p-8">
-        <CodeEditor defaultCode={defaultCode} onCodeChange={setCode} />
-        <Button
-          className="mt-4 px-4 py-2"
-          onClick={() => {
-            const url = encodeTextToUrlHash(code)
-            navigator.clipboard.writeText(url)
-            alert("URL copied to clipboard!")
-          }}
-        >
-          Create URL for Code Snippet
-        </Button>
-      </div>
-      <div className="w-1/2 p-8">
-        <Tabs defaultValue="pcb">
-          <TabsList>
-            <TabsTrigger value="pcb">PCB</TabsTrigger>
-            <TabsTrigger value="cad">3D</TabsTrigger>
-            <TabsTrigger value="table">Table</TabsTrigger>
-          </TabsList>
-          <TabsContent value="pcb">
-            <div className="mt-4 h-[500px]">
-              <PCBViewer soup={circuitJson} />
-            </div>
-          </TabsContent>
-          <TabsContent value="cad">
-            <div className="mt-4 h-[500px]">
-              <CadViewer soup={circuitJson as any} />
-            </div>
-          </TabsContent>
-          <TabsContent value="table">
-            <div className="mt-4 h-[500px]">
-              <SoupTableViewer elements={circuitJson as any} />
-            </div>
-          </TabsContent>
-        </Tabs>
-        <textarea className="w-full h-32 bg-red-100" value={message} />
+    <div className="flex flex-col">
+      <EditorNav code={code} />
+      <div className="flex">
+        <div className="w-1/2 p-2 border-r border-gray-200 bg-gray-50">
+          <CodeEditor defaultCode={defaultCode} onCodeChange={setCode} />
+        </div>
+        <div className="w-1/2 p-2">
+          <Tabs defaultValue="pcb">
+            <TabsList>
+              <TabsTrigger value="pcb">PCB</TabsTrigger>
+              <TabsTrigger value="cad">3D</TabsTrigger>
+              <TabsTrigger value="table">Table</TabsTrigger>
+            </TabsList>
+            <TabsContent value="pcb">
+              <div className="mt-4 h-[500px]">
+                <PCBViewer soup={circuitJson} />
+              </div>
+            </TabsContent>
+            <TabsContent value="cad">
+              <div className="mt-4 h-[500px]">
+                <CadViewer soup={circuitJson as any} />
+              </div>
+            </TabsContent>
+            <TabsContent value="table">
+              <div className="mt-4 h-[500px]">
+                <SoupTableViewer elements={circuitJson as any} />
+              </div>
+            </TabsContent>
+          </Tabs>
+          <textarea className="w-full h-32 bg-red-100" value={message} />
+        </div>
       </div>
     </div>
   )
---
+++ src/components/EditorNav.tsx
@@ -0,0 +1,117 @@
+import {
+  ChevronDown,
+  Copy,
+  Download,
+  Edit2,
+  Eye,
+  Maximize2,
+  Package,
+  Share,
+  Share2,
+  Sidebar,
+  SidebarClose,
+} from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { OpenInNewWindowIcon } from "@radix-ui/react-icons"
+import { encodeTextToUrlHash } from "@/lib/encodeTextToUrlHash"
+
+export default function EditorNav({ code }: { code: string }) {
+  return (
+    <nav className="flex items-center justify-between px-2 py-3 border-b border-gray-200 bg-white text-sm border-t">
+      <div className="flex items-center space-x-1">
+        {/* <span className="text-base font-semibold">»</span> */}
+        <span className="text-md font-semibold">
+          seveibar/new-snippet-0f83a4
+        </span>
+        <Button variant="ghost" size="icon" className="h-6 w-6 ml-1">
+          <OpenInNewWindowIcon className="h-3 w-3 text-gray-500" />
+        </Button>
+      </div>
+      <div className="flex items-center space-x-2">
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button variant="outline" size="sm" className="h-6 px-2 text-xs">
+              <Download className="mr-1 h-3 w-3" />
+              Download
+              <ChevronDown className="ml-1 h-3 w-3" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem className="text-xs">
+              <Download className="mr-1 h-3 w-3" />
+              <span className="flex-grow mr-6">Download TSX</span>
+              <span className="text-[0.6rem] opacity-80 bg-blue-500 text-white font-mono rounded-md px-1 text-center py-0.5 mr-1">
+                tsx
+              </span>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="text-xs">
+              <Download className="mr-1 h-3 w-3" />
+              <span className="flex-grow  mr-6">Download 3D Model</span>
+              <span className="text-[0.6rem] bg-green-500 opacity-80 text-white font-mono rounded-md px-1 text-center py-0.5 mr-1">
+                stl
+              </span>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="text-xs">
+              <Download className="mr-1 h-3 w-3" />
+              <span className="flex-grow mr-6">Download Footprint</span>
+              <span className="text-[0.6rem] bg-orange-500 opacity-80 text-white font-mono rounded-md px-1 text-center py-0.5 mr-1">
+                kicad_mod
+              </span>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="text-xs">
+              <Download className="mr-1 h-3 w-3" />
+              <span className="flex-grow mr-6">Download KiCad Zip</span>
+              <span className="text-[0.6rem] bg-orange-500 opacity-80 text-white font-mono rounded-md px-1 text-center py-0.5 mr-1">
+                kicad_*
+              </span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <Button
+          variant="outline"
+          size="sm"
+          className="h-6 px-2"
+          onClick={() => {}}
+        >
+          <Copy className="mr-1 h-3 w-3" />
+          <span className="font-mono text-[0.6rem]">
+            tsci add seveibar/new-snippet-0f83a4
+          </span>
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          className="h-6 px-2 text-xs"
+          onClick={() => {
+            const url = encodeTextToUrlHash(code)
+            navigator.clipboard.writeText(url)
+            alert("URL copied to clipboard!")
+          }}
+        >
+          <Share className="mr-1 h-3 w-3" />
+          Copy URL
+        </Button>
+        <Button variant="outline" size="sm" className="h-6 px-2 text-xs">
+          <Eye className="mr-1 h-3 w-3" />
+          Public
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          className="h-6 px-2 text-[0.6rem] bg-green-500 text-white hover:bg-green-600 hover:text-white"
+        >
+          Running
+        </Button>
+        <Button variant="ghost" size="icon" className="h-6 w-6">
+          <Sidebar className="h-3 w-3" />
+        </Button>
+      </div>
+    </nav>
+  )
+}
