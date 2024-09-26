b0f41b8696d54755117828f94ab604ee5b214647
seveibar <seveibar@gmail.com>
2024-09-25 19:47:15 -0700

    adapt to vite plugin, fixes weird delay issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite"
+import { defineConfig, Plugin } from "vite"
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { getNodeHandler } from "winterspec/adapters/node"
@@ -7,24 +7,39 @@ import { getNodeHandler } from "winterspec/adapters/node"
 import winterspecBundle from "./dist/bundle.js"
 const fakeHandler = getNodeHandler(winterspecBundle as any, {})
 
+function apiFakePlugin(): Plugin {
+  return {
+    name: "api-fake",
+    configureServer(server) {
+      server.middlewares.use((req, res, next) => {
+        if (req.url?.startsWith("/api/")) {
+          fakeHandler(req, res)
+        } else {
+          next()
+        }
+      })
+    },
+  }
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), apiFakePlugin()],
   define: {
     global: {},
   },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:9999",
-        changeOrigin: true,
-        configure: (proxy, _options) => {
-          proxy.on("proxyReq", (proxyReq, req, res) => {
-            return fakeHandler(req, res)
-          })
-        },
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "http://localhost:9999",
+  //       changeOrigin: true,
+  //       configure: (proxy, _options) => {
+  //         proxy.on("proxyReq", (proxyReq, req, res) => {
+  //           return fakeHandler(req, res)
+  //         })
+  //       },
+  //     },
+  //   },
+  // },
   build: {
     minify: false,
     terserOptions: {
