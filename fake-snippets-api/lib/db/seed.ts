9a0d5246063181e6a5f2467dfd145cb6ffaa0149
seveibar <seveibar@gmail.com>
2024-10-14 10:52:21 -0700

    add replication in seed

--- fake-snippets-api/lib/db/seed.ts
+++ fake-snippets-api/lib/db/seed.ts
@@ -62,4 +62,27 @@ exports.A555Timer = A555Timer;
     snippet_type: "package",
     description: "A simple package with an A555 Timer component",
   })
+
+  // Add a snippet that outputs a square waveform using the a555timer
+
+  db.addSnippet({
+    name: "seveibar/a555timer-square-wave",
+    unscoped_name: "a555timer-square-wave",
+    owner_name: "seveibar",
+    code: `
+import { A555Timer } from "@tsci/seveibar.a555timer"
+
+export default () => (
+  <A555Timer name="U1" />
+)
+`.trim(),
+    dts: "export declare const MyChip: (props: {\n    name: string;\n}) => any;\n",
+    compiled_js:
+      '"use strict";\n\nObject.defineProperty(exports, "__esModule", {\n  value: true\n});\nexports.MyChip = void 0;\nvar _seveibar = require("@tsci/seveibar.a555timer");\nconst MyChip = props => /*#__PURE__*/React.createElement(_seveibar.A555Timer, {\n  name: "U1"\n});\nexports.MyChip = MyChip;',
+    created_at: new Date().toISOString(),
+    updated_at: new Date().toISOString(),
+    snippet_type: "package",
+    description:
+      "A simple package that outputs a square waveform using the a555timer",
+  })
 }
