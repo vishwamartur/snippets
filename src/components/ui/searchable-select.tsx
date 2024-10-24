062b8452f9cc3318dde0b3f89e8fcc443fb159a0
Andrii Balitskyi <84702959+andrii-balitskyi@users.noreply.github.com>
2024-10-24 19:29:40 +0200

    Fix SearchableSelect options width (#124)

--- src/components/ui/searchable-select.tsx
+++ src/components/ui/searchable-select.tsx
@@ -61,7 +61,7 @@ export const SearchableSelect: React.FC<SearchableSelectProps> = ({
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-full p-0">
+      <PopoverContent className="w-[var(--radix-popover-trigger-width)] p-0">
         <Command>
           <CommandInput placeholder={getPlaceholder()} />
           <CommandList>
